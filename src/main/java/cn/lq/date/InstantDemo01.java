0d01ff9975992212e49a7c76c1afd14e95caef1a
dark <1429535947@qq.com>
2022-10-31 14:36:56 +0800

    update

--- src/main/java/cn/lq/date/InstantDemo01.java
+++ src/main/java/cn/lq/date/InstantDemo01.java
@@ -1,18 +1,8 @@
 package cn.lq.date;
 
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.net.http.HttpClient;
-import java.time.Duration;
+
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 public class InstantDemo01 {
     public static void main(String[] args) {
--- src/main/java/cn/lq/spring/bean/Person.java
+++ src/main/java/cn/lq/spring/bean/Person.java
@@ -1,5 +1,6 @@
 package cn.lq.spring.bean;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.Lifecycle;
@@ -7,11 +8,9 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Component()
-public class Person implements InitializingBean {
+public class Person implements InitializingBean, DisposableBean {
     private String name;
     private String age;
     @Autowired
@@ -41,19 +40,15 @@ public class Person implements InitializingBean {
         this.car = car;
     }
 
-    @PostConstruct
-    public void init() {
-        System.out.println(this.getClass().getSimpleName() + "：被初始化");
-    }
-
-    @PreDestroy
-    public void destroy() {
-        System.out.println(this.getClass().getSimpleName() + "：被销毁");
-    }
 
     @Override
     public void afterPropertiesSet() throws Exception {
         //do something
+        System.out.println("init");
     }
 
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy");
+    }
 }
