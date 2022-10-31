1c24605738380395e2c77834a88556209e9138b8
dark <1429535947@qq.com>
2022-10-31 15:04:38 +0800

    update

--- src/main/java/cn/lq/spring/bean/Person.java
+++ src/main/java/cn/lq/spring/bean/Person.java
@@ -16,6 +16,17 @@ public class Person implements InitializingBean, DisposableBean {
     @Autowired
     private Car car;
 
+    @Autowired
+    private Bike bike;
+
+    public Bike getBike() {
+        return bike;
+    }
+
+    public void setBike(Bike bike) {
+        this.bike = bike;
+    }
+
     public String getName() {
         return name;
     }
